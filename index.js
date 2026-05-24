2c3a370f7d1c69f458ebde9f8744dcc04f8491ce
AISHWARYA K <kaishu953@gmail.com>
2026-05-24 15:43:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var a="aihswrya";
+console.log(a);
+let name="aishu";
+console.log(name);
+const pi=3.14;
+console.log(pi);
+
+
+let str="aihsu";
+console.log(str);
+let num=233;
+console.log(num);
+let isTrue=true;
+console.log(isTrue);
+let big=246347357353n;
+console.log(big);
+let val=null;
+console.log(val);
+let value;
+console.log(value);
+
+
+let ob={
+    name:"aihu",
+    age:20,
+    email:"aihsuys23@gmai.com"
+};
+
+let arr=[1,2,3,3,3];
+
+function app(){
+    console.log("function is working");
+}
+
+app();
